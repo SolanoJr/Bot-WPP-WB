e06ee9e4839420083f58e105cd5cfdd9f4d6b509
SolanoJr <solanojr@example.com>
2026-08-04 16:54:20 -0300

    FIX: Adicionar env_file ao ecosystem.config.js para carregar variáveis de ambiente

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -8,6 +8,7 @@ module.exports = {
     autorestart: true,
     watch: false,
     max_memory_restart: '1G',
+    env_file: '/home/solanojr/bot-wpp/.env',
     env: {
       NODE_ENV: 'production'
     }
